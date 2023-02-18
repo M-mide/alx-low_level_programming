76a2fbc1c6a3d9d6db80054922e71ebe58059d8d
Makanju Wonderful <mayomidemakanju@gmail.com>
2023-02-17 16:36:47 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-#!/bin/bash
-#include <stdio.h> 
+#include <stdio.h>
+/**
+ * main - Program prints a line with put funtion
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
